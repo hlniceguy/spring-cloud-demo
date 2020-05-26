ecb07104af338df12390fd5f18c4e2264e4dbe4f
hl234 <hlniceguy@aliyun.com>
2020-05-26 21:27:25 +0800

    添加accessfilter

---
+++ spring-cloud-route/api-gateway-zuul/src/main/java/com/study/filter/AccessFilter.java
@@ -0,0 +1,77 @@
+package com.study.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * 说明：
+ * 作者：hanll-a
+ * 时间：2020/5/26 15:43
+ */
+@Component
+public class AccessFilter extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccessFilter.class);
+
+    /**
+     * 过滤器的类型，决定该过滤器在请求的哪个生命周期中执行
+     * pre
+     * route
+     * post
+     * error
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    /**
+     * 过滤器的执行顺序。当请求在一个阶段中存在多个过滤器时，需要根据该方法返回的值来依次执行
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    /**
+     * 判断该过滤器是否需要被执行。
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 过滤器的具体逻辑
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.info("Send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+
+        Object accessToken = request.getParameter("token");
+        if (null == accessToken) {
+            logger.warn("access token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        logger.info("access token ok");
+        return null;
+
+    }
+}
